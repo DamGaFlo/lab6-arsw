b1cde00fe77753ced08841618c077b011c4ea4a7
JCPosso <juancamiloposso01@gmail.com>
2021-09-25 17:48:46 -0500

    first part ok

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,9 +1,26 @@
-var app = apimock;
-var app  = (function(){
-    var currentAuthor;
-    var puntos =[];
-    return{
-
-    }
-
-})();
+app = (function (){
+    var api = apimock;
+    var editTable = function (blueprints) {
+        if(blueprints){
+            $("#table tbody").empty();
+            var i =0;
+            $("#table tbody").append(
+                blueprints.map(function(blueprint){
+                i+= blueprint.points.length;
+                return  '<tr><td id="name">'+ blueprint.name +
+                        '</td><td id="points">'+ blueprint.points.length +
+                        '</td><td ><button class="btn btn-success"> Open</button>'+
+                        '</td></tr>';
+                })
+            );
+            $("#puntosLabel").text(i);
+            $("#autorLabel").text(author);
+        }
+    };
+    return {
+            plansAuthor: function () {
+                author = document.getElementById("autor").value;
+                api.getBlueprintsByAuthor(author,editTable);
+                }
+        };
+    })();
