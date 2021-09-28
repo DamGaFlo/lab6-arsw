46341968974db3c614b0db5f77a557a205e74e6a
JCPosso <juancamiloposso01@gmail.com>
2021-09-27 19:55:18 -0500

    draw canvas

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,5 @@
 app = (function (){
-    var api = apimock;
+    var api = apiclient;
     var editTable = function (blueprints) {
         if(blueprints){
             $("#table tbody").empty();
@@ -9,18 +9,38 @@ app = (function (){
                 i+= blueprint.points.length;
                 return  '<tr><td id="name">'+ blueprint.name +
                         '</td><td id="points">'+ blueprint.points.length +
-                        '</td><td ><button class="btn btn-success"> Open</button>'+
+                        '</td><td ><button class="btn btn-success" onclick="app.drawcanvas(\''+
+                        blueprint.name+'\',\''+blueprint.author+'\')"> Opens </button>'+
                         '</td></tr>';
                 })
             );
             $("#puntosLabel").text(i);
             $("#autorLabel").text(author);
         }
+    }
+    var editCanvas = function (blueprint) {
+        if (blueprint){
+            var c = document.getElementById("canvas");
+            var ctx = c.getContext("2d");
+            var points = blueprint.points;
+            ctx.beginPath();
+            console.log(points);
+            ctx.fillStyle = "#fff";
+            ctx.fillRect(0, 0,c.width,c.height);
+            ctx.moveTo(points[0].x,points[0].y);
+            points.forEach( function(point){
+                ctx.lineTo(point.x,point.y)
+            });
+            ctx.stroke();
+        }
     };
     return {
             plansAuthor: function () {
                 author = document.getElementById("autor").value;
                 api.getBlueprintsByAuthor(author,editTable);
-                }
+                },
+            drawcanvas: function(name,author){
+                api.getBlueprintsByNameAndAuthor(name,author,editCanvas);
+          }
         };
     })();
